3ba1dd0928c20345d3c2076d11b632186f924176
jessicaas883-wq <jessicaas883@gmail.com>
2026-09-24 23:13:47 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { Analytics } from '@vercel/analytics/next'
 import type { Metadata, Viewport } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
@@ -32,7 +31,6 @@ export default function RootLayout({
     <html lang="pt-BR" className={`dark scroll-smooth ${poppins.variable}`}>
       <body className="bg-background font-sans antialiased">
         {children}
-        {process.env.NODE_ENV === 'production' && <Analytics />}
       </body>
     </html>
   )
