f24dfa79b71bfa57bde51bf107107f81256bd776
jessicaas883-wq <jessicaas883@gmail.com>
2026-09-24 23:29:07 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+'use client'
+
+import { LoadingScreen } from '@/components/loading-screen'
+
 export default function Page() {
-  return (
-    <main>
-      <h1>Teste</h1>
-    </main>
-  )
+  return <LoadingScreen onComplete={() => {}} />
 }
